4f301af2972625ea8352f6cc59bf65ad675d75da
ajcamp67 <alfiejcampbell67@gmail.com>
2024-10-01 18:03:15 +0100

    Moved testing files to the testing folder.

---
+++ main/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import customtkinter as ctk
+import yfinance as yf
+
+# STOCK VARIABLES
+GSPC = ''
+GSPC_history = ''
+# COMMODITY VARIABLES
+gold = ''
+goldHistory = ''
+
+# Fetch data on financial instruments
+GSPC = yf.Ticker('^GSPC')
+GSPC_history = GSPC.history(period="max")
+GSPC_history = pd.DataFrame(GSPC_history)
+
+gold = yf.Ticker('GC=F')
+goldHistory = gold.history(period="max")
+goldHistory = pd.DataFrame(goldHistory)
+
+# Create CSVs for financial instruments
+GSPC_history.to_csv('S&P500history.csv')
+goldHistory.to_csv('goldHistory.csv')
--- testing/yahoofinancetest.py
+++ testing/yahoofinancetest.py
@@ -1,5 +1,11 @@
 import yfinance as yf
+import pandas as pd
+import numpy as np
 
 spfiveData = yf.Ticker("^GSPC")
 # print(spfiveData.info)
-print(spfiveData.history(period="max"))
+# print(spfiveData.history(period="ytd"))
+spfiveHistory = spfiveData.history(period="ytd")
+spfiveHistory = pd.DataFrame(spfiveHistory)
+print(spfiveHistory)
+spfiveHistory.to_csv('SP500.csv')
